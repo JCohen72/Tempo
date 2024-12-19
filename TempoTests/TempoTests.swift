0fb42602e8e0e56be58c0cddb1305d4dea568159
Joey Cohen <jecohen72@gmail.com>
2024-12-18 23:34:14 -0500

    Initial Commit

---
+++ TempoTests/TempoTests.swift
@@ -0,0 +1,16 @@
+//
+//  TempoTests.swift
+//  TempoTests
+//
+//  Created by Joey Cohen on 12/18/24.
+//
+
+import Testing
+
+struct TempoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
